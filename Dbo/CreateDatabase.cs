20c2c9e4e6e619a319329289ceaa990eb95b58c1
THEDARKLORD <tomas12345tomas@gmail.com>
2019-09-19 23:45:58 +0300

    Fix file system

---
+++ Dbo/CreateDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace Dbo
+{
+    class CreateDatabase
+    {
+        private static readonly string SqlConnectionString = DatabaseExecutor.connetionString;
+
+        private static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(SqlConnectionString);
+            var fileEntries = Directory.GetFiles("Scripts");
+            conn.Open();
+
+            foreach (var fileEntry in fileEntries)
+            {
+                File.OpenRead(fileEntry);
+                string script = File.ReadAllText(fileEntry);
+                script = script.Replace("\r\n", " ");
+                using (SqlCommand command = new SqlCommand(script,conn))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    Console.WriteLine("DataBase was updated successfully");
+                }
+            }
+            
+            conn.Close();
+            Console.WriteLine("DataBase is ready");
+        }
+    }
+}
---
+++ ServerCallFromApp/DataManipulations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace NotexServerCallFromApp
+{
+    public static class DataManipulations
+    {
+        private const string Url = "https://localhost:44380/api/";
+
+        public static string GetDataFromServer(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(Url+url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsStringAsync().Result;
+                client.Dispose();
+                return dataObjects;
+            }
+
+            client.Dispose();
+
+            return null;
+        }
+        public static void PostDataToServer(string url,string data)
+        {
+            HttpClient client = new HttpClient();
+            var payload = data;
+
+            HttpContent c = new StringContent(payload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(Url + url,c).Result;
+            client.Dispose();
+        }
+    }
+}
