2494b756626987c9fa95c4cf91027e1d8f3e19c0
Dovile <dovmartink@gmail.com>
2019-10-27 16:35:17 +0200

    unit test for CreateUser in sign up (but not GetForm - wrong branch title)

---
+++ AppUnitTests/SignUp_Create_User_Should.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App;
+using AppUnitTests.TestHelpers;
+using Xunit;
+
+namespace AppUnitTests
+{
+    public class SignUp_Create_User_Should
+    { 
+        [Theory]
+        [AutoMoqData]
+         public async void TestUserIsCreated(SignUpFormManager signUpFormManager)
+        {
+           string name = StringGenerator.RandomString(4);
+           string email = StringGenerator.RandomEmail();
+                  
+           Assert.Equal(2, await signUpFormManager.CreateUser(name, email, StringGenerator.RandomString(6)));                            //successful
+           Assert.Equal(2, await signUpFormManager.CreateUser(StringGenerator.RandomString(4), email, StringGenerator.RandomString(6))); //existing email
+           Assert.Equal(1, await signUpFormManager.CreateUser(name, StringGenerator.RandomEmail(), StringGenerator.RandomString(6)));    //existing username
+           // BUG IN CreateUser!!! Allows creation with duplicate emails and returns 2 instead of 0 and duplicate user names (returns 2 instead of 1)
+        }
+    }
+}
---
+++ AppUnitTests/StringGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using App;
+
+namespace AppUnitTests
+{
+    public class StringGenerator
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(System.Linq.Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomEmail()
+        {
+            return string.Format("qa{0:0000}@test.com", random.Next(10000));
+        }
+    }
+}
