1560400e9f81b66ad849d82831b637c277adb798
Dovile <dovmartink@gmail.com>
2019-10-27 17:11:02 +0200

    Fixed previous botched PR from wrong account

--- AppUnitTests/SignUp_Create_User_Should.cs
+++ AppUnitTests/SignUp_Create_User_Should.cs
@@ -16,10 +16,11 @@ namespace AppUnitTests
            string name = StringGenerator.RandomString(4);
            string email = StringGenerator.RandomEmail();
                   
-           Assert.Equal(2, await signUpFormManager.CreateUser(name, email, StringGenerator.RandomString(6)));                            //successful
-           Assert.Equal(0, await signUpFormManager.CreateUser(StringGenerator.RandomString(4), email, StringGenerator.RandomString(6))); //existing email
-           Assert.Equal(1, await signUpFormManager.CreateUser(name, StringGenerator.RandomEmail(), StringGenerator.RandomString(6)));    //existing username
+           Assert.Equal((int)CreateUserReturn.SUCCESS, await signUpFormManager.CreateUser(name, email, StringGenerator.RandomString(6)));                            //successful
+           Assert.Equal((int)CreateUserReturn.EMAIL_TAKEN, await signUpFormManager.CreateUser(StringGenerator.RandomString(4), email, StringGenerator.RandomString(6))); //existing email
+           Assert.Equal((int)CreateUserReturn.USERNAME_TAKEN, await signUpFormManager.CreateUser(name, StringGenerator.RandomEmail(), StringGenerator.RandomString(6)));    //existing username
            // BUG IN CreateUser!!! Allows creation with duplicate emails and returns 2 instead of 0 and duplicate user names (returns 2 instead of 1)
         }
+      
     }
 }
