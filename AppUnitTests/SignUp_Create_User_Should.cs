953977e79f00b1db316a18363178fe519aa4de35
Dovile <dovmartink@gmail.com>
2019-10-27 16:43:03 +0200

    Fixed

--- AppUnitTests/SignUp_Create_User_Should.cs
+++ AppUnitTests/SignUp_Create_User_Should.cs
@@ -17,7 +17,7 @@ namespace AppUnitTests
            string email = StringGenerator.RandomEmail();
                   
            Assert.Equal(2, await signUpFormManager.CreateUser(name, email, StringGenerator.RandomString(6)));                            //successful
-           Assert.Equal(2, await signUpFormManager.CreateUser(StringGenerator.RandomString(4), email, StringGenerator.RandomString(6))); //existing email
+           Assert.Equal(0, await signUpFormManager.CreateUser(StringGenerator.RandomString(4), email, StringGenerator.RandomString(6))); //existing email
            Assert.Equal(1, await signUpFormManager.CreateUser(name, StringGenerator.RandomEmail(), StringGenerator.RandomString(6)));    //existing username
            // BUG IN CreateUser!!! Allows creation with duplicate emails and returns 2 instead of 0 and duplicate user names (returns 2 instead of 1)
         }
