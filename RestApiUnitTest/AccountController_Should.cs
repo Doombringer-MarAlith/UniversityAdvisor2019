0338bb3df6a4b798981b40419197bbfd516f5342
THEDARKLORD <tomas12345tomas@gmail.com>
2019-10-28 18:38:08 +0200

    Visiems aciu nebereikia testu

--- RestApiUnitTest/AccountController_Should.cs
+++ RestApiUnitTest/AccountController_Should.cs
@@ -1,17 +1,12 @@
 ﻿using AutoFixture.Xunit2;
 using Dbo;
-using Microsoft.AspNetCore.Http;
-using Models.Models;
 using Debugger;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System;
-using Xunit;
-using RestApiUnitTest.TestHelpers;
+using Models.Models;
 using Moq;
+using Newtonsoft.Json;
 using RestApi.Controllers;
-using System.Net;
-using Microsoft.VisualBasic;
+using RestApiUnitTest.TestHelpers;
+using Xunit;
 
 namespace RestApiUnitTest
 {
@@ -24,7 +19,49 @@ namespace RestApiUnitTest
             var serializedAccount = JsonConvert.SerializeObject(account);
             database.Setup(x => x.ReturnAccount(id)).Returns(account);
             var response = controller.Get(id);
-            database.Verify(x=>x.ReturnAccount(id),Times.Once);
+            database.Verify(x => x.ReturnAccount(id), Times.Once);
+            Assert.NotNull(response);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("3")]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("0874645365132af35f131sadf2sad1fsa35fd543fa1sd0a13f15sa456sad1f35sad1fs2d1fds31fs5d4f3sda12f1sadf2asdf2s3ad1fsaf")]
+        [InlineData("-0874645365132af35f131sadf2sad1fsa35fd543fa1")]
+        [InlineData("0874645365132af35f131sadf2sad1fsa35fd543fa1")]
+        [InlineData("efsaiah")]
+        [InlineData("fdasf0")]
+        [InlineData("fsad0")]
+        [InlineData("fdasewrq0")]
+        [InlineData("fsewqewrqerwqrwqrad0")]
+        [InlineData(",,,,,,,,,,,,,,,,,,,,,,,")]
+        [InlineData("............................")]
+        [InlineData("rw                                                     qf0")]
+        [InlineData("few0")]
+        [InlineData("____________________________________")]
+        [InlineData("few0")]
+        [InlineData("rwqe0")]
+        [InlineData("ewqfewfq0")]
+        [InlineData("/////////////////////////////")]
+        [InlineData("0fewqf                                                                                                                 wqf")]
+        [InlineData("rweqqr             w0")]
+        [InlineData("0wrqerwqe")]
+        [InlineData("                                                         k                                                     0")]
+        [InlineData("????????????????????????????????????")]
+        public void Get_DontCrash_WithValue(string id)
+        {
+            Mock<ILogger> logger = new Mock<ILogger>();
+            Account account = new Account();
+            Mock<IDatabaseExecutor> database = new Mock<IDatabaseExecutor>();
+            AccountController controller = new AccountController(new DatabaseExecutor(new Logger()), new Logger());
+            var serializedAccount = JsonConvert.SerializeObject(account);
+
+            database.Setup(x => x.ReturnAccount(id)).Returns(account);
+            var response = controller.Get(id);
             Assert.NotNull(response);
         }
     }
