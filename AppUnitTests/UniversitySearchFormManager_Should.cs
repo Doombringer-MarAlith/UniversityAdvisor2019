a8b83fa6a0697e18cd4839eb8fd08d81896862ee
BluePowerPlayer <50358308+BluePowerPlayer@users.noreply.github.com>
2019-10-28 17:31:09 +0200

    Added tests (#34)
    * Added tests
    
    * Code Formatting
    
    * PR fixes

---
+++ AppUnitTests/UniversitySearchFormManager_Should.cs
@@ -0,0 +1,67 @@
+﻿using App;
+using App.FormManagers;
+using AppUnitTests.TestHelpers;
+using AutoFixture.Xunit2;
+using Models.Models;
+using Moq;
+using Newtonsoft.Json;
+using ServerCallFromApp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppUnitTests
+{
+    public class UniversitySearchFormManager_Should
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task GetUniversities_IfExist(
+            List<University> universities, string name,
+            [Frozen] Mock<IDataManipulations> dataManipulations,
+            [Frozen] Mock<FormManagerData> formManagerData,
+            UniversitySearchFormManager sut)
+        {
+            string jsonString = JsonConvert.SerializeObject(universities);
+            string url = $"university/{name}";
+            dataManipulations.Setup(x => x.GetDataFromServer(It.IsAny<string>())).ReturnsAsync(jsonString);
+            var response = await sut.GetUniversities(name);
+            dataManipulations.Verify(x => x.GetDataFromServer(It.Is<string>(y => y.Equals(url))), Times.Once);
+            Assert.Equal(universities.Select(uni => uni.Name).ToList(), response);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetUniversities_IfNotExist(List<University> universities,
+            string name,
+            [Frozen] Mock<IDataManipulations> dataManipulations,
+            [Frozen] Mock<FormManagerData> formManagerData,
+            UniversitySearchFormManager sut)
+        {
+            string jsonString = JsonConvert.SerializeObject(universities);
+            string url = $"university/{name}";
+            string ur = null;
+            dataManipulations.Setup(x => x.GetDataFromServer(It.IsAny<string>())).ReturnsAsync(ur);
+            var response = await sut.GetUniversities(name);
+            dataManipulations.Verify(x => x.GetDataFromServer(It.Is<string>(y => y.Equals(url))), Times.Once);
+            Assert.Equal(new List<string>(), response);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetUniversities_IfUnexpectedReturnObject_DoNotCrash(List<University> universities,
+            string name,
+            [Frozen] Mock<IDataManipulations> dataManipulations,
+            [Frozen] Mock<FormManagerData> formManagerData,
+            UniversitySearchFormManager sut)
+        {
+            string jsonString = JsonConvert.SerializeObject(universities);
+            string url = $"university/{name}";
+            dataManipulations.Setup(x => x.GetDataFromServer(It.IsAny<string>())).ReturnsAsync("");
+            var response = await sut.GetUniversities(name);
+            dataManipulations.Verify(x => x.GetDataFromServer(It.Is<string>(y => y.Equals(url))), Times.Once);
+            Assert.Equal(new List<string>(), response);
+        }
+    }
+}
