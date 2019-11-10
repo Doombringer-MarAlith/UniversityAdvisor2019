a5dbd6a2c279d6cd0593d4263fa929e17afd7f61
BluePowerPlayer <50358308+BluePowerPlayer@users.noreply.github.com>
2019-11-10 16:52:27 +0200

    Web (#40)
    * Nuggets fucked up added Web in progress
    
    * FIxed everything :D
    
    * minor pr fix

---
+++ DboExecutor/IDatabaseExecutor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Models.Models;
+
+namespace DboExecutor
+{
+    public interface IDatabaseExecutor
+    {
+        void CreateAccount(Account account);
+        string CheckAccountEmail(string email);
+        string CheckAccountUsername(string username);
+        Account ReturnAccount(string id);
+        string ReturnAccountGuid(string email, string password);
+        void DeleteAccount(string id);
+        List<Review> ReturnReviews(string Guid, int guidType);
+        void CreateUniversity(University university);
+        List<University> ReturnUniversities(string name);
+        void CreateFaculty(Faculty faculty);
+        List<Faculty> ReturnFaculties(string guid);
+        Review ReturnReview(string Guid);
+        void CreateReview(Review review);
+    }
+}
