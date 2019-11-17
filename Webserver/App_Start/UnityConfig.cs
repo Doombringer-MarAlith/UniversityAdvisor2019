09204782e27369e5ea55bb114379f239de572c2f
Doombringer <ryti.lev@gmail.com>
2019-11-17 20:42:42 +0200

    Add DI, repository class + university search view

---
+++ Webserver/App_Start/UnityConfig.cs
@@ -0,0 +1,20 @@
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+using Webserver.Data;
+using Webserver.Repositories;
+
+namespace Webserver
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            container.RegisterType<ApplicationDbContext, ApplicationDbContext>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
---
+++ Webserver/Controllers/UniversitiesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Webserver.Data;
+using Webserver.Models;
+
+namespace Webserver.Controllers
+{
+    public class UniversitiesController : Controller
+    {
+        private ApplicationDbContext _dbContext { get; set; }
+
+        public UniversitiesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: Universities
+        public async Task<ActionResult> Index()
+        {
+            return View(await _dbContext.Universities.ToListAsync());
+        }
+
+        // GET: Universities/Search/{text}
+        public async Task<ActionResult> Search(string text)
+        {
+            if (text == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var universityList = await _dbContext.Universities.ToListAsync();
+            return View("Index", universityList.FindAll(uni => uni.Name.Contains(text)));
+        }
+
+        // GET: Universities/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            University university = await _dbContext.Universities.FindAsync(id);
+            if (university == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(university);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
