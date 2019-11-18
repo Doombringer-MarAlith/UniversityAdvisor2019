a5d18edcd5cf30a013f01ed3455507387bfa90b1
juliusBaranauskas <juliu100@gmail.com>
2019-11-18 21:09:07 +0200

    Web scraper v2 (#55)
    * webscraper migrated to .net framework, adding threads
    
    * missed some changes
    
    * progress console info, new country
    
    * final touches

---
+++ WebScraper/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+
+namespace WebScraper.Models
+{
+    [JsonObject]
+    class Account
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Guid")]
+        public string Guid { get; set; }
+    }
+}
