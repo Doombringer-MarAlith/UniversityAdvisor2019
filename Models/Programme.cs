3e19a3a0bbdddf9a525d6f0a3733c32c0fa1c628
juliusBaranauskas <juliu100@gmail.com>
2019-11-22 09:46:09 +0200

    added Programme model

---
+++ Models/Programme.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    public class Programme
+    {
+        public string Name { get; set; }
+
+        public int FacultyId { get; set; }
+
+        public int UniversityId { get; set; }
+
+        public int Id { get; set; }
+    }
+}
