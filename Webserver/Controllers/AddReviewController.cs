3ee0b65f0cd3d43520564460a9e43a62ce317e6f
juliusBaranauskas <juliu100@gmail.com>
2019-11-23 22:56:51 +0200

    Add review (#65)
    * adding reviews, programme added

--- Webserver/Controllers/AddReviewController.cs
+++ Webserver/Controllers/AddReviewController.cs
@@ -1,17 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Models;
+using Webserver.Data.Repositories;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 
 namespace Webserver.Controllers
 {
+    [Authorize]
     public class AddReviewController : Controller
     {
+        IReviewRepository _reviewRepository;
+        public AddReviewController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
         // GET: AddReview
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult UniversityReview(int universityId)
+        {
+            ViewBag.universityId = universityId;
+            return View();
+        }
+
+        public ActionResult FacultyReview(int facultyId)
+        {
+            ViewBag.facultyId = facultyId;
+            return View();
+        }
+
+        public ActionResult ProgrammeReview(int programmeId)
+        {
+            ViewBag.programmeId = programmeId;
+            return View();
+        }
+
+        // POST
+        public async Task<ActionResult> ReviewUniversity(Review model, int id)
+        {
+            model.UniversityId = id;
+            if(model.Value > 5 || model.Value < 1)
+            {
+                model.Value = 5;
+            }
+
+            model.UserId = User.Identity.GetUserId();
+            _reviewRepository.Add(model);
+            await _reviewRepository.Commit();
+            return RedirectToAction("Details", "Universities", new { id = id });
+        }
+
+        // POST
+        public async Task<ActionResult> ReviewFaculty(Review model, int id)
+        {
+            model.FacultyId = id;
+            if (model.Value > 5 || model.Value < 1)
+            {
+                model.Value = 5;
+            }
+
+            model.UserId = User.Identity.GetUserId();
+            _reviewRepository.Add(model);
+            await _reviewRepository.Commit();
+            return RedirectToAction("Details", "Faculties", new { id = id });
+        }
+
+        // POST
+        public async Task<ActionResult> ReviewProgramme(Review model, int id)
+        {
+            model.ProgrammeId = id;
+            if (model.Value > 5 || model.Value < 1)
+            {
+                model.Value = 5;
+            }
+
+            model.UserId = User.Identity.GetUserId();
+            _reviewRepository.Add(model);
+            await _reviewRepository.Commit();
+            return RedirectToAction("Details", "Programmes", new { id = id });
         }
     }
 }
---
+++ Webserver/Controllers/ProgrammesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Webserver.Data.Repositories;
+using Models;
+
+namespace Webserver.Controllers
+{
+    public class ProgrammesController : Controller
+    {
+        private IProgrammeRepository _repository;
+
+        public ProgrammesController(IProgrammeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult Index(int facultyId)
+        {
+            ViewBag.FacultyId = facultyId;
+            IEnumerable<Programme> programmes = _repository.GetMany(programme => programme.FacultyId == facultyId);
+            return View(programmes);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Programme programme = _repository.GetById(id);
+            if (programme == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.ProgrammeId = id;
+            ViewBag.FacultyId = programme.FacultyId;
+            return View(programme);
+        }
+    }
+}
