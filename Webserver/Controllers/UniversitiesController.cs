65c5cda1ee1bd7e169b63bd59b0ebc2099e6601a
Dovile <dovmartink@gmail.com>
2019-11-25 16:43:59 +0200

    added charts to university details page

--- Webserver/Controllers/UniversitiesController.cs
+++ Webserver/Controllers/UniversitiesController.cs
@@ -2,6 +2,9 @@
 using System.Net;
 using System.Web.Mvc;
 using Webserver.Data.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using ASPNET_MVC_Samples.Models;
 
 namespace Webserver.Controllers
 {
@@ -33,6 +36,7 @@ namespace Webserver.Controllers
         }
 
         // GET: Universities/Details/{id}
+       
         public ActionResult Details(int id)
         {
             University university = _repository.GetById(id);
@@ -41,8 +45,20 @@ namespace Webserver.Controllers
                 return HttpNotFound();
             }
 
-            ViewBag.UniversityId = id;
+            List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(1, 22),                   
+                new DataPoint(2, 36),
+                new DataPoint(3, 42),
+                new DataPoint(4, 51),
+                new DataPoint(5, 46),
+            };  //sioje vietoje reikia is duombazes paskaiciuot kiek yra review su value 1, kiek su value 2 ir iki 5.
+            // bet bedele tame kad tai yra review controller o ne UniversitiesController
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
             return View(university);
         }
+
+      
     }
 }
