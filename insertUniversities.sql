89934fa6a65082178da7ff486f04d7e2d1a07613
TomasKireilis <tomas12345tomas@gmail.com>
2019-11-25 17:50:03 +0200

    Deleted files

--- insertUniversities.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO University(Guid, Name, Descripition, Year)
-VALUES
-('1', 'Vilniaus Universitetas', 'Pirmasis ir diziausias universitetas Lietuvoje', 1579 ),
-('2', 'Kauno Technologijos Universitetas', 'Vienas didziausiu technologiju universitetu Baltijos salyse', 1922),
-('3', 'Vilniaus Gedimino Technikos Universitetas', ' ', 1956),
-('4', 'Vilniaus kolegija', 'Aukstoji valstybine mokykla', 2000);
