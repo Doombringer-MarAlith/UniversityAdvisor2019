7146a2e841c2ee6de082ede043dddaa0caf4283f
TomasKireilis <tomas12345tomas@gmail.com>
2019-11-26 17:14:59 +0200

    Added unit tests

---
+++ Webserver.Tests/Controllers/ReviewControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+using Webserver.Controllers;
+using Webserver.Data.Infrastructure;
+using Webserver.Data.Repositories;
+using Xunit;
+
+namespace Webserver.Tests.Controllers
+{
+    public class ReviewControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            var controller = new AddReviewController(new ReviewRepository(new DatabaseFactory()));
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
