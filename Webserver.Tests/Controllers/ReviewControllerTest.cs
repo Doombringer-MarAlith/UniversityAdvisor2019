220553a453bff50163a6b9f844fbac22c1140794
Doombringer <ryti.lev@gmail.com>
2019-12-02 22:55:06 +0200

    Cleanup, pagination

--- Webserver.Tests/Controllers/ReviewControllerTest.cs
+++ Webserver.Tests/Controllers/ReviewControllerTest.cs
@@ -1,7 +1,5 @@
 ﻿using AutoFixture.Xunit2;
-using Models;
 using Moq;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using Webserver.Controllers;
 using Webserver.Data.Repositories;
@@ -16,6 +14,7 @@ namespace Webserver.Tests.Controllers
         public void Index([Frozen] Mock<IReviewRepository> reviewRepository)
         {
             var sut = new AddReviewController(reviewRepository.Object);
+
             // Act
             ViewResult result = sut.Index() as ViewResult;
 
@@ -28,6 +27,7 @@ namespace Webserver.Tests.Controllers
         public void UniversityReview([Frozen] Mock<IReviewRepository> reviewRepository)
         {
             var sut = new AddReviewController(reviewRepository.Object);
+
             // Act
             ViewResult result = sut.UniversityReview(15) as ViewResult;
 
@@ -40,6 +40,7 @@ namespace Webserver.Tests.Controllers
         public void FacultyReview([Frozen] Mock<IReviewRepository> reviewRepository)
         {
             var sut = new AddReviewController(reviewRepository.Object);
+
             // Act
             ViewResult result = sut.FacultyReview(14) as ViewResult;
 
@@ -52,6 +53,7 @@ namespace Webserver.Tests.Controllers
         public void ProgrammeReview([Frozen] Mock<IReviewRepository> reviewRepository)
         {
             var sut = new AddReviewController(reviewRepository.Object);
+
             // Act
             ViewResult result = sut.ProgrammeReview(14) as ViewResult;
 
---
+++ Webserver/Models/ViewModels/Pagination/Pager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Webserver.Models.ViewModels.Pagination
+{
+    public class Pager
+    {
+        public Pager(int totalItems, int? page, int pageSize = 10)
+        {
+            var totalPageCount = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+            var currentPage = page ?? 1;
+            var startPage = currentPage - 5;
+            var endPage = currentPage + 4;
+
+            if (startPage <= 0)
+            {
+                endPage -= startPage - 1;
+                startPage = 1;
+            }
+
+            if (endPage > totalPageCount)
+            {
+                endPage = totalPageCount;
+
+                if (endPage > 10)
+                {
+                    startPage = endPage - 9;
+                }
+            }
+
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPageCount = totalPageCount;
+            StartPage = startPage;
+            EndPage = endPage;
+        }
+
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPageCount { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+    }
+}
