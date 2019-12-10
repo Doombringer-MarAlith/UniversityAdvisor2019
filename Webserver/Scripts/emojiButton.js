54ca224c23ef483413ebdeed3bb9219a215da17b
juliusBaranauskas <juliu100@gmail.com>
2019-12-10 21:22:20 +0200

    add emoji support for review

---
+++ Webserver/Scripts/emojiButton.js
@@ -0,0 +1,12 @@
+﻿var input = document.querySelector('#emojiArea');
+var picker = new EmojiButton({
+    position: 'right-start'
+});
+
+picker.on('emoji', emoji => {
+    input.value += emoji;
+});
+
+input.addEventListener('click', () => {
+    picker.pickerVisible ? picker.hidePicker() : picker.showPicker(input);
+});
